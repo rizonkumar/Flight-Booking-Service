d230f4ed6fb6186c3399123e41958dc8af042907
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-10-27 10:37:14 +0530

    Feat: Added constants data

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,13 @@
 const MESSAGES = {
-  SUCCESS: {},
-  ERROR: {},
+  SUCCESS: {
+    BOOKING_CREATED: "Booking created successfully",
+  },
+  ERROR: {
+    BOOKING_FAILED: "Unable to create booking",
+    REQUIRED_NO_SEATS: "Required number of seats is not available",
+    NO_OF_SEATS_EXCEEDS_AVAILABLE_SEATS:
+      "Cannot book - Requested seats ({{requested}}) exceed available seats ({{available}})",
+  },
   INFO: {
     REQUEST_RECEIVED: "Request received and being processed",
   },
