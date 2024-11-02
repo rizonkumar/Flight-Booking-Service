d1360573bc2f78f12313fae66dfde5f0484c8323
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-02 08:41:15 +0530

    Feat: Implemented Booking Payment System

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -14,6 +14,27 @@ class BookingRespository extends CrudRepository {
     const response = await Booking.create(data, { transaction: transaction });
     return response;
   }
+
+  async get(data) {
+    const response = await this.model.findByPk(data);
+    if (!response) {
+      throw new Error(MESSAGES.ERROR.BOOKING_NOT_FOUND, StatusCodes.NOT_FOUND);
+    }
+    return response;
+  }
+
+  async update(id, data, transaction) {
+    const response = await this.model.update(
+      data,
+      {
+        where: {
+          id: id,
+        },
+      },
+      { transaction: transaction }
+    );
+    return response;
+  }
 }
 
 module.exports = BookingRespository;
